46a209af63f6c0a97089ba0eb47b43e65120ea6f
Philipp Kuboth <philipp@kuboth.dev>
2023-12-09 11:37:15 +0100

    Pleasant

---
+++ 09/mirage_madness.1.py
@@ -0,0 +1,10 @@
+with open("input.txt", "r") as oasis_and_sand_instability_sensor_data:
+    total_sum_of_new_elements = 0
+    for line in oasis_and_sand_instability_sensor_data:
+        history = [int(value) for value in line.split()]
+        derivatives = [[history[i] - history[i - 1] for i in range(1, len(history))]]
+        while any(derivatives[-1]):
+            derivatives.append([derivatives[-1][i] - derivatives[-1][i - 1] for i in range(1, len(derivatives[-1]))])
+        new_element = sum([derivate[-1] for derivate in derivatives]) + history[-1]
+        total_sum_of_new_elements += new_element
+    print(total_sum_of_new_elements)
